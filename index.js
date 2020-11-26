aec42298e02f3537c1b8468c0432b1919df4828b
Pamela Sergio <mariefraulein.sergi@hcl.com>
2020-11-27 03:33:46 +0800

    Code cleanup

--- index.js
+++ index.js
@@ -1,50 +1,15 @@
-
-
-/*
-var http = require('http');
-var express = require('express');
-var app = express();
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-//app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-*/
-
-
-/*
-var express = require('express'),
-    fs = require('fs'),
-    app = express();
-
-var app = express();
-
-var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
-
-app.get('/', function(req, res) {
-    res.send('Hello from NodeJS  at '+ new Date());
-});
-
-
-app.listen(8080, ip);
-
-module.exports = app;
-*/
-
 var express = require('express'),
  app = express(),
  port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || '8080',
  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+app.use('/', function (req, res, next) {
+ console.log("A new request received at " + Date.now());
+ next();
+});
+
 app.get('/', function (req, res) {
  res.send('Hello from Jalapenos!\n ' + new Date());
 });
 
-app.listen(port, ip),
-function(){
-  console.log('Express server listening on port ' + app.get('port'))
-};
+app.listen(port, ip);
